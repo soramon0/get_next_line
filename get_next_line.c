ce686df40e741b5d8a6dd54bc89986be53451bce
soramon0 <23435312+soramon0@users.noreply.github.com>
2025-01-09 15:31:34 +0100

    do not keep track of returned line (WIP)

--- get_next_line.c
+++ get_next_line.c
@@ -12,95 +12,90 @@
 
 #include "get_next_line.h"
 
-static void	cleanup(char **tupile)
+static void *cleanup(char *buf)
 {
-	if (tupile[0] != NULL)
-		free(tupile[0]);
-	if (tupile[1] != NULL)
-		free(tupile[1]);
-	tupile[0] = NULL;
-	tupile[1] = NULL;
+	if (buf != NULL)
+		free(buf);
+	buf = NULL;
+	return (NULL);
 }
 
-static char	*get_line(char **tupile, size_t end)
+static char	*get_line(char *buf, size_t end)
 {
 	char	*tmp;
 	size_t	i;
 
 	i = 0;
-	tmp = ft_strndup(tupile[0], end);
+	tmp = ft_strndup(buf, end);
 	if (!tmp)
-		return (cleanup(tupile), NULL);
-	if (tupile[1] != NULL)
-		free(tupile[1]);
-	tupile[1] = tmp;
-	while (tupile[0][end])
-		tupile[0][i++] = tupile[0][end++];
-	while (tupile[0][i])
-		tupile[0][i++] = '\0';
+		return (cleanup(buf));
+	while (buf[end])
+		buf[i++] = buf[end++];
+	while (buf[i])
+		buf[i++] = '\0';
 	return (tmp);
 }
 
-static int	expand_buf(char **tupile, size_t buf_size)
+static char *expand_buf(char *buf, size_t buf_size)
 {
-	char	*tmp;
+	char	*new_buf;
 	size_t	i;
 
 	i = 0;
-	tmp = (char *)malloc(buf_size + BUFFER_SIZE + 1);
-	if (!tmp)
-		return (-1);
-	while (tupile[0][i])
+	new_buf = (char *)malloc(buf_size + BUFFER_SIZE + 1);
+	if (!new_buf)
+		return (cleanup(buf));
+	while (buf[i])
 	{
-		tmp[i] = tupile[0][i];
+		new_buf[i] = buf[i];
 		i++;
 	}
-	tmp[i] = '\0';
-	free(tupile[0]);
-	tupile[0] = tmp;
-	return (0);
+	new_buf[i] = '\0';
+	free(buf);
+	return (new_buf);
 }
 
-static char	*work(int fd, char **tupile)
+static char	*work(int fd, char *buf)
 {
 	size_t	buf_size;
 	ssize_t	bytes;
 	ssize_t	nl_pos;
 
-	if (!tupile[0][0])
+	if (!buf[0])
 	{
-		bytes = read(fd, tupile[0], BUFFER_SIZE);
+		bytes = read(fd, buf, BUFFER_SIZE);
 		if (!bytes)
-			return (cleanup(tupile), NULL);
+			return (cleanup(buf));
 	}
-	nl_pos = ft_strchr(tupile[0], '\n');
+	nl_pos = ft_strchr(buf, '\n');
 	while (nl_pos == -1)
 	{
-		buf_size = ft_strlen(tupile[0]);
-		if (expand_buf(tupile, buf_size) == -1)
-			return (cleanup(tupile), NULL);
-		bytes = read(fd, &tupile[0][buf_size], BUFFER_SIZE - buf_size);
+		buf_size = ft_strlen(buf);
+		buf = expand_buf(buf, buf_size);
+		if (!buf)
+			return (cleanup(buf));
+		bytes = read(fd, &buf[buf_size], BUFFER_SIZE - buf_size);
 		if (bytes == 0)
-			return (get_line(tupile, buf_size));
+			return (get_line(buf, buf_size));
 		if (!bytes)
-			return (cleanup(tupile), NULL);
-		nl_pos = ft_strchr(tupile[0], '\n');
+			return (cleanup(buf));
+		nl_pos = ft_strchr(buf, '\n');
 	}
-	return (get_line(tupile, nl_pos + 1));
+	return (get_line(buf, nl_pos + 1));
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*tupile[2];
+	static char	*buf;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (!tupile[0])
+	if (!buf)
 	{
-		tupile[0] = (char *)malloc(BUFFER_SIZE + 1);
-		if (!tupile[0])
+		buf = (char *)malloc(BUFFER_SIZE + 1);
+		if (!buf)
 			return (NULL);
-		tupile[0][0] = '\0';
+		buf[0] = '\0';
 	}
-	return (work(fd, tupile));
+	return (work(fd, buf));
 }
