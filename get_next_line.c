afda589f7675b05965e6b6cb1702c3821b1af2fe
soramon0 <23435312+soramon0@users.noreply.github.com>
2025-01-09 12:59:15 +0100

    feat: check before freeing

--- get_next_line.c
+++ get_next_line.c
@@ -14,23 +14,28 @@
 
 static void	cleanup(char **tupile)
 {
-	free(tupile[0]);
-	free(tupile[1]);
+	if (tupile[0] != NULL)
+		free(tupile[0]);
+	if (tupile[1] != NULL)
+		free(tupile[1]);
+	tupile[0] = NULL;
+	tupile[1] = NULL;
 }
 
-static char	*get_line(char **tupile, ssize_t nl_pos)
+static char	*get_line(char **tupile, size_t end)
 {
 	char	*tmp;
 	size_t	i;
 
 	i = 0;
-	tmp = ft_strndup(tupile[0], ++nl_pos);
+	tmp = ft_strndup(tupile[0], end);
 	if (!tmp)
 		return (cleanup(tupile), NULL);
-	free(tupile[1]);
+	if (tupile[1] != NULL)
+		free(tupile[1]);
 	tupile[1] = tmp;
-	while (tupile[0][nl_pos])
-		tupile[0][i++] = tupile[0][nl_pos++];
+	while (tupile[0][end])
+		tupile[0][i++] = tupile[0][end++];
 	while (tupile[0][i])
 		tupile[0][i++] = '\0';
 	return (tmp);
@@ -76,12 +81,12 @@ static char	*work(int fd, char **tupile)
 			return (cleanup(tupile), NULL);
 		bytes = read(fd, &tupile[0][buf_size], BUFFER_SIZE - buf_size);
 		if (bytes == 0)
-			return (get_line(tupile, buf_size - 1));
+			return (get_line(tupile, buf_size));
 		if (!bytes)
 			return (cleanup(tupile), NULL);
 		nl_pos = ft_strchr(tupile[0], '\n');
 	}
-	return (get_line(tupile, nl_pos));
+	return (get_line(tupile, nl_pos + 1));
 }
 
 char	*get_next_line(int fd)
@@ -91,8 +96,11 @@ char	*get_next_line(int fd)
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	if (!tupile[0])
+	{
 		tupile[0] = (char *)malloc(BUFFER_SIZE + 1);
-	if (!tupile[0])
-		return (NULL);
+		if (!tupile[0])
+			return (NULL);
+		tupile[0][0] = '\0';
+	}
 	return (work(fd, tupile));
 }
